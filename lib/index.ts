e5e769072078cf672e6d020b8df73fb643917e43
thetinyspark <contact@the-tiny-spark.com>
2021-07-14 23:34:06 +0200

    add example

--- lib/index.ts
+++ lib/index.ts
@@ -1,25 +1,37 @@
-import AssetsManager, { JSON_TYPE } from "./AssetsManager";
+import Bitmap from "./display/Bitmap";
+import Stage from "./display/Stage";
+
+function renderLoop(stage:Stage){
+   stage.nextFrame();
+   stage.getChildren()[0].rotation++;
+   stage.getChildren()[0].updateMatrix();
+   window.requestAnimationFrame(
+      ()=>{
+         renderLoop(stage);
+      }
+   );
+}
 
 async function init() {
+   const stage:Stage = new Stage();
+   const bmp:Bitmap = new Bitmap();
+   document.body.appendChild(stage.getCanvas());
 
-   try {
-      const manager: AssetsManager = new AssetsManager();
-      const list = await manager.load("data.json", JSON_TYPE, "list") as any[];
+   stage.getCanvas().width = stage.width = 600;
+   stage.getCanvas().height = stage.height = 480;
+   bmp.texture = document.getElementById("img") as HTMLImageElement;
+   bmp.width = 100;
+   bmp.height = 100;
+   bmp.x = 100; 
+   bmp.y = 100;
+   bmp.scaleX = 2;
+   bmp.scaleY = 2;
+   bmp.rotation = 45;
+   bmp.updateMatrix();
 
-      list.forEach(
-         (current) => {
-            manager.queue(current.uri, current.type, current.alias);
-         }
-      );
+   stage.addChild(bmp); 
 
-      const data = await manager.loadQueue();
-      document.body.appendChild(manager.get("kirby"));
-      document.body.appendChild(manager.get("curve"));
-   } 
-   catch (error) {
-      console.log(error);
-   }
-
+   renderLoop(stage);
 }
 
 window.addEventListener("load", init);
