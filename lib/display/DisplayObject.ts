f716f17a07a547f36edf39de3b920fb41a551d28
thetinyspark <contact@the-tiny-spark.com>
2021-07-14 15:52:21 +0200

    add 2d transformation matrices

--- lib/display/DisplayObject.ts
+++ lib/display/DisplayObject.ts
@@ -1,15 +1,40 @@
+import { mat2, mat2d, mat3 } from "gl-matrix";
 import { IDisplayObject } from "./IDisplayObject";
 import { IDisplayObjectContainer } from "./IDisplayObjectContainer";
 
 export default class DisplayObject implements IDisplayObject{
 
+    public worldMatrix: mat2d = mat2d.create();
+    public matrix: mat2d = mat2d.create();
     public x: number = 0;
     public y: number = 0;
+    public scaleX:number = 1;
+    public scaleY:number = 1;
+    public rotation:number = 0;
     public width: number = 0;
     public height: number = 0;
     public parent:IDisplayObjectContainer|null = null;
 
+
+    public updateMatrix():void{
+        mat2d.identity(this.matrix);
+        mat2d.translate(this.matrix, this.matrix, [this.x, this.y]);
+        mat2d.rotate(this.matrix, this.matrix, this.rotation * (Math.PI / 180));
+        mat2d.scale(this.matrix, this.matrix, [this.scaleX, this.scaleY]);
+
+        if( this.parent === null ){
+            this.worldMatrix = this.matrix;
+        }
+        else{
+            this.parent.updateMatrix();
+            mat2d.multiply(this.worldMatrix, this.parent.worldMatrix, this.matrix);
+        }
+    }
+
     public render(context: CanvasRenderingContext2D): void {
-        throw new Error("Method not implemented.");
+        const matrix = this.worldMatrix;
+        context.save();
+        context.transform(matrix[0],matrix[1],matrix[2],matrix[3], matrix[4], matrix[5]);
+        context.restore();
     }
 }
