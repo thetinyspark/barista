f5f26a727bfc43d38057f6fe9046e8277780b0df
thetinyspark <contact@the-tiny-spark.com>
2022-01-07 23:08:05 +0100

    fix exports

--- lib/index.ts
+++ lib/index.ts
@@ -8,6 +8,7 @@ export * from "./core/texture/index";
 export * from "./core/utils/index";
 export * from "@thetinyspark/tiny-observer";
 
+export * from "./sdk/common/utils/index";
 export * from "./sdk/common/model/node/index";
 export * from "./sdk/common/model/space/partitioning/grid/index";
 export * from "./sdk/common/model/space/storage/index";
